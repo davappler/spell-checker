9bdb1c03aabdea627ffa58136001db8ea6cb6102
Davinder <davibaweja@gmail.com>
2021-12-17 21:39:01 +0000

    added react router dom

--- basicenglish/src/App.js
+++ basicenglish/src/App.js
@@ -1,8 +1,13 @@
+import { Route, Switch } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route path="/" exact>
+        </Route>
+      </Switch>
     </div>
   );
 }

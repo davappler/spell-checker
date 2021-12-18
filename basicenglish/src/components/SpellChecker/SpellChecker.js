449409ea2a538752cd440cf255f3d41fec5fc574
Davinder <davibaweja@gmail.com>
2021-12-18 09:38:17 +0000

    added the 850 words into a json file

--- basicenglish/src/components/SpellChecker/SpellChecker.js
+++ basicenglish/src/components/SpellChecker/SpellChecker.js
@@ -1,65 +1,76 @@
 import React,{useState} from "react";
-// import { TextField } from '@material-ui/core';
 import "./SpellChecker.scss";
+import arrayOf850words from "../../Assets/words.json";
 
 export function SpellChecker(){
 
   const [currentSentence,setCurrentSentence]=useState("")
   const [correctSentence,setCorrectSentence]=useState()
 
-
+  
   function handleChange(e){
     setCurrentSentence(e.target.value)
   }
 
+
+
+  // This is a boolean function, it takes a word and checks if that word is in the list of 850 BASIC words
   function checkWordExistence(word){
     let punctuationMarks=[".","?","!",",",":",";","-","[","]","{","}","(",")","'",'"',"...",""]
-    let finalWordArray=[]
-    let finalWord=""
+    let finalWordArray=[] // We will break the word into characters and then store in this array
+    let finalWord="" // From the array above we will restore our word.
     word=word.toLowerCase();
     let wordArray=word.split("")
 
-    wordArray.map((character,index)=>{
+    wordArray.map((character)=>{
       if(!punctuationMarks.includes(character))
         finalWordArray.push(character)  
     })
 
-    let arrayOf850words=["my","name","is","davinder","singh"];
     finalWord=finalWordArray.join("")
-    let value =arrayOf850words.includes(finalWord)
-    return value
+    let result =arrayOf850words.includes(finalWord)
+    return result
   }
 
+
+
+  // This function will hightlight the incorrect words(words that are not in the BASIC 850 words)
   function highlightIncorrect(incorrectWordsArray, userInput) {
     let result = userInput;
     incorrectWordsArray.forEach(incorrectWord=>{
         result = result.replaceAll(incorrectWord, `<span className="incorrect-word">${incorrectWord}</span>`);
-        console.log(result);
       })
     return result;
   }
 
+
+
+
+  // This function takes the user input sentence and breaks our problem to check the spelling one by one.
   function checkSpellings(currentSentence){
     let wordsArray=currentSentence.split(" ")
     let incorrectWordsArray=[]
     let exist
     wordsArray.map((word)=>{
-      exist=checkWordExistence(word) // This will check if word exists in 850 english words or not
+      exist=checkWordExistence(word) // This will check if word exists in 850 english words or not.
       if(!exist)
       {
         incorrectWordsArray.push(word)
       }
     })
 
-      let highLightedText=highlightIncorrect(incorrectWordsArray,currentSentence)
-      setCorrectSentence(highLightedText)
-
+    let highLightedText=highlightIncorrect(incorrectWordsArray,currentSentence)
+    setCorrectSentence(highLightedText)
   }
 
-      function handleSubmit(e){
-      e.preventDefault ();
-      checkSpellings(currentSentence)
-    }
+
+
+
+
+  function handleSubmit(e){
+    e.preventDefault ();
+    checkSpellings(currentSentence)
+  }
 
   return( 
   <div className="spell-check-container">
